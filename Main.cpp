4b5137c34cfa80c2db467eab965e343ae001223d
Emily Radtke <eradtke5902@it01-124990.ad.ppsnet>
2023-03-08 13:24:13 -0800

    TestingEigen

--- Main.cpp
+++ Main.cpp
@@ -3,14 +3,25 @@
 #include <opencv2/core.hpp>
 
 using Eigen::MatrixXd;
-using Eigen::VectorXd;
+using Eigen:: VectorXd;
 
 int main()
 {
-	MatrixXd m = MatrixXd::Random(3, 3);
-	m = (m + MatrixXd::Constant(3, 3, 1.2)) * 50;
-	std::cout << "m =" << std::endl << m << std::endl;
-	VectorXd v(3);
-	v << 1, 2, 3;
-	std::cout << "m * v =" << std::endl << m * v << std::endl;
+    /* Example 1 for Understanding Eigen
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1)  = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+    */
+
+    MatrixXd m = MatrixXd::Random(3,3);
+   // m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    std::cout << "m =" << std::endl << m << std::endl;
+    VectorXd v(3);
+    v << 1 , 1, 1;
+    //row 1 gets multiplyed by the number in spot 0
+    //weirdly the decimals are slightly off from my school calc to the computer values
+    std::cout << "m * v =" << std::endl << m * v << std::endl;
 }
---
+++ Test-Image-Processing.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Emily Radtke on 3/8/23.
+//
+// This file is made for image processing
+// Goal --> get computer to recognise it is receiving an image
+
+
+
